4e129e54be8d90bc65dc9f044e046fc6b441c28b
Guy Allard <guyallard01@gmail.com>
2016-12-15 15:04:57 +0100

    barebones project

---
+++ README.md
@@ -0,0 +1,13 @@
+GFF/GTF Iterator
+==============
+A very simple python iterator for parsing gff files
+
+
+**Installation**  
+from within this directory, run  
+```pip install .```
+
+
+**Usage**  
+```TODO
+    example usage here```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="gff_iterator",
+      version="0.0.1",
+      description="Simple iterator to parse records from a gff file",
+      author="Guy Allard",
+      author_email="guyallard01 AT gmail DOT com",
+      url="https://github.com/GuyAllard/gff_iterator",
+      license="MIT",
+      platforms=['any'],
+      packages=["gff_iterator"])
